fe9077c4552d499082f44cabb83060ee9d0332dc
Vlad <zitarvlad11@gmail.com>
2022-10-29 08:41:16 +0600

    Homework for urok 14 - object methods

--- src/Book.java
+++ src/Book.java
@@ -3,10 +3,12 @@ import java.util.Objects;
 public class Book {
     private String name;
     private int yearOfPublication;
+    private String author;
 
-    public Book(String name, int yearOfPublication) {
+    public Book(String name, int yearOfPublication, String author) {
         this.name = name;
         this.yearOfPublication = yearOfPublication;
+        this.author = author;
     }
 
     public String getName() {
@@ -17,12 +19,17 @@ public class Book {
         return this.yearOfPublication;
     }
 
+    public String getAuthor() {
+        return this.author;
+    }
+
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
     }
 
     public String toString() {
-        return "Название книги - " + this.name + " Год издания - " + yearOfPublication;
+        return "Название книги - " + this.name + " Год издания - " + this.yearOfPublication + " Автор книги - " +
+                this.author;
     }
 
     @Override
@@ -30,13 +37,14 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name);
+        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, yearOfPublication);
+        return Objects.hash(name, yearOfPublication, author);
     }
 }
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,10 @@ public class Main {
         //Домашняя работа
         System.out.println("Домашняя работа");
 
-        Book grokAlgorithms = new Book("GrokAlgorithms", 2017);
+        Book grokAlgorithms = new Book("GrokAlgorithms", 2017, "Aditya Bhargava");
         System.out.println("grokAlgorithms.name = " + grokAlgorithms.getName());
         System.out.println("grokAlgorithms.yearOfPublication = " + grokAlgorithms.getYearOfPublication());
+        System.out.println("grokAlgorithms.author = " + grokAlgorithms.getAuthor());
         grokAlgorithms.setYearOfPublication(2019);
         System.out.println("grokAlgorithms.getYearOfPublication() = " + grokAlgorithms.getYearOfPublication());
         Author aditya = new Author("Аditya", "Bhargava");
